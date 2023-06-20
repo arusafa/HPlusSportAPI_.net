7fbd5e7b33c714c3ff2f4cbb3f5e4eab94facf19
arusafa <arusafacollege@gmail.com>
2023-06-19 23:52:45 -0400

    Products has been added

---
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using HPlusSport.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HPlusSport.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopContext _context; //db = _context
+
+        public ProductsController(ShopContext context)
+        {
+            _context = context;
+
+            _context.Database.EnsureCreated();
+        }
+
+        /*
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _context.Products.ToArray();
+        }
+        */
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        /*
+        // Ok method HttpStatus 200
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetAllProducts()
+        {
+            var products = _context.Products.ToArray();
+            return Ok(products);
+        }
+        */
+
+        [HttpGet("{id}")] // [HttpGet] [Route("/api/products/{id}")]
+        public ActionResult GetProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+            
+            if (product == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(product);
+        }
+    }
+}
